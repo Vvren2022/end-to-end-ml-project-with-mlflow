99a95d724f9327b3b1c70d0c9790e89d25e1a065
Viren Vaviya <vrenvaviya2022@gmail.com>
2025-12-28 18:09:13 +0530

    data ongestion added

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 from src.mlProject import logger
 
 
-logger.info("Starting ML Project")
+# logger.info("Starting ML Project")
 
-# from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-# from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
-# from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
+from src.mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+# from src.mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+# from src.mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+# from src.mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+# from src.mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
 
-# STAGE_NAME = "Data Ingestion stage"
-# try:
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-#    data_ingestion = DataIngestionTrainingPipeline()
-#    data_ingestion.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
